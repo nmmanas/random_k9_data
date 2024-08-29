b6ba8769d9b94a6d9c5f20f32cc4650dcec8fba6
Manas <nmmanas@gmail.com>
2024-08-29 17:01:10 +0530

    added health check api

--- app.py
+++ app.py
@@ -19,5 +19,10 @@ def get_random_data():
     return jsonify(random_data)
 
 
+@flask_app.route("/health", methods=["GET"])
+def health_check():
+    return 'OK'
+
+
 if __name__ == "__main__":
     flask_app.run(host="0.0.0.0", port=5000)
